710b1a84409f2044183af87a9df8a5c90802a743
mcc-fft <49076990+mcc-fft@users.noreply.github.com>
2019-04-09 22:55:35 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create functions which can cache certain computation results and retrieve them when required.  
+##Use lexical scoping to cache values between the Global environment and sub environments.
+## There are two functions required below, and they are patterned very closely after the makeVector and cachemean functions in the example documentation.
 
-## Write a short comment describing this function
-
+## makecacheMatrix is a function that creates a special matrix object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse 
+        getinv <- function() inv  
+        list(set = set, get = get, 
+             setinv = setinv, 
+             getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## cachesolve computes the inverse of the special matrix returned by makecachematrix. 
+##If the inverse has already been calculated and has not changed, then retrieve the inverse matrix from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
